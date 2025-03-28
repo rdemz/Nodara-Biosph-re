93ad869be17ec8d745b8720d2974f028ac5a2951
rdemz <r.demeillez@live.fr>
2025-03-28 22:32:45 +0100

    Create lib.rs

---
+++ bug_bounty_and_community/src/lib.rs
@@ -0,0 +1,6 @@
+//! Module de bug bounty et communauté pour Nodara BIOSPHÈRE QUANTIC.
+
+/// Fonction d'exemple pour vérifier que le module se compile correctement.
+pub fn example() {
+    println!("Bug bounty module loaded.");
+}
